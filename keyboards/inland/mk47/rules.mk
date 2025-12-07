78081653de85bd85e0f2aa9721f064940b87c853
Efrain Saldana <esaldana@gmail.com>
2025-12-07 13:03:59 -0500

    Add rules.mk for Inland MK47 to enable VIAL and define MCU

---
+++ keyboards/inland/mk47/rules.mk
@@ -0,0 +1,14 @@
+# Define the core QMK/ChibiOS platform for the WB32 chip
+CHIBIOS_PLATFORM = WB32
+CHIBIOS_MCU = WB32F103VET6
+
+# Enable Link Time Optimization (LTO) for smaller firmware size (recommended)
+LTO_ENABLE = yes
+
+# Enable VIAL protocol
+VIAL_ENABLE = yes
+# VIAL_INSECURE allows for VIAL to work without cryptographic checks, which is common for custom builds
+VIAL_INSECURE = yes
+
+# The keyboard's default keymap folder should be 'default'
+KEYMAPS = default
